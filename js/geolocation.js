e6d92c909a427abeac4c634f92ce93ef9dbc19ac
chfouka <chfouka@di.unipi.it>
2015-02-14 23:31:55 +0100

    test

--- js/geolocation.js
+++ js/geolocation.js
@@ -76,7 +76,6 @@ function resolveLocationFromUser() {
 
 function heart() {
     document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
-
 }
 
 window.onload = function () { getLocation(); }
